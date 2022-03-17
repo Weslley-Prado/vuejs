114e1da916eefded084846037968d8754210e012
Weslley Prado <weslley.prado73@gmail.com>
2022-03-16 21:56:23 -0300

    Creating calculator

---
+++ calculatorvue/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+
+@font-face {
+    font-family: "RobotoMono";
+    src: url("./fonts/Roboto-Thin.ttf");
+}
+* {
+    font-family: "RobotoMono", monospace;
+}
+body {
+    margin: 0;
+}
+
+#app {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    color: #fff;
+    background: linear-gradient(to right, rgb(83, 105, 118), rgb(41, 46, 73));
+}
+</style>
